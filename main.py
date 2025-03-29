8202ce9862b5e74f29c5cc2e4632c38a6dcf358c
SHIVADITYA BHATTACHARYA <shivadityabhattacharya.11c@gmail.com>
2025-03-30 01:32:49 +0530

    modified to work with project2 for ga2q6. future modification will make it work with ga2q9 as well

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import json
 from typing import List
+import pandas as pd
+import requests
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -11,21 +14,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get('/api')
+@app.get('/api/ga2q6')
 def getmarks(name: List[str] = Query(default=[])):
-    lom = []
-    with open('q-vercel-python.json', "r") as json_file:
-        data = json.load(json_file)
-        for n in name:
-            print(n)
-            for entity in data:
-                if n == entity['name']:
-                    lom.append(entity['marks'])
-                    break
-    return {
-        "marks": lom
+    data = {
+        "name": name
     }
+    response = requests.post('http://localhost:8000/api/p2ga2q6', json=data)
+    return response.json()
 
+@app.get("/api/ga2q10")
+def get_students(class_: list[str] = Query(None, alias="class")):
+    """
+    Fetch student data from the CSV. If 'class' query parameters are provided,
+    filter students by those classes.
+    """
+    if class_:
+        filtered_df = df[df["class"].isin(class_)]
+    else:
+        filtered_df = df
+
+    # Convert to dictionary list
+    students = filtered_df.to_dict(orient="records")
+    return {"students": students}
 
 @app.get('/')
 def check():
